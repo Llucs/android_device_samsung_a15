e4bd0b060e63ac1107bd5d6489c581298d5761bd
Llucs <104036495+Llucs@users.noreply.github.com>
2025-11-07 15:18:34 -0300

    Atualizar o crdroid_a15g99.mk

--- crdroid_a15g99.mk
+++ crdroid_a15g99.mk
@@ -4,22 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+# Inherit from the base device configuration
+$(call inherit-product, device/samsung/a15g99/device.mk)
 
-# Inherit some common crDroid stuff.
+# Inherit common crDroid configuration
 $(call inherit-product, vendor/crdroid/config/common.mk)
 
-# Device identifier. This must come after all inclusions.
+# Device identifiers
 PRODUCT_NAME := crdroid_a15g99
 PRODUCT_DEVICE := a15g99
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A155M
+PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=$(PRODUCT_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_NAME)
 
 # crDroid specific overrides
 CRDROID_BUILD_TYPE := OFFICIAL
