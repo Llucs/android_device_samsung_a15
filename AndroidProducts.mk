4f5d0dd4bcfbbf279d88002cbe0d2200bb117af5
Llucs <c307lucas@gmail.com>
2025-12-23 12:05:54 -0300

    Organização: Criando base neutra a15.mk e modularizando AndroidProducts

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/a15.mk \
     $(LOCAL_DIR)/crdroid_a15.mk
---
+++ a15.mk
@@ -0,0 +1,9 @@
+# Herda as configurações gerais do dispositivo
+$(call inherit-product, device/samsung/a15/device.mk)
+
+# Configurações de Identidade do Produto
+PRODUCT_NAME := a15
+PRODUCT_DEVICE := a15
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A155M
+PRODUCT_MANUFACTURER := samsung
