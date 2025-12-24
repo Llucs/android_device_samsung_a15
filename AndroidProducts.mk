26ac88b4ea367e50627a54307137cc801eb97f3a
Llucs <104036495+Llucs@users.noreply.github.com>
2025-12-24 16:29:39 -0300

    Atualizar o AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,10 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/a15.mk \
     $(LOCAL_DIR)/crdroid_a15.mk \
-    $(LOCAL_DIR)/lineage_a15.mk
+    $(LOCAL_DIR)/lineage_a15.mk \
+    $(LOCAL_DIR)/yaap_a15.mk
 
 COMMON_LUNCH_CHOICES := \
     crdroid_a15-userdebug \
-    lineage_a15-userdebug
+    lineage_a15-userdebug \
+    yaap_a15-userdebug
