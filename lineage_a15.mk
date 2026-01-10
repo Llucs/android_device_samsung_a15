c9d759dcdac69149d3044adaf5435017b95c511d
Llucs <104036495+Llucs@users.noreply.github.com>
2026-01-09 21:25:55 -0300

    Atualizar o lineage_a15.mk

--- lineage_a15.mk
+++ lineage_a15.mk
@@ -1,7 +1,8 @@
-# Herdar as configurações da LineageOS
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Herdar a base universal do dispositivo
+# Inherit from the base device configuration first
 $(call inherit-product, device/samsung/a15/a15.mk)
 
+# Inherit some common Lineage stuff (last, so it can override/extend device configs)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Define the product name for LineageOS builds
 PRODUCT_NAME := lineage_a15
