e72596b21740859f010c88c85cd068ed1d03a6b3
Llucs <104036495+Llucs@users.noreply.github.com>
2026-01-09 21:24:57 -0300

    Atualizar o crdroid_a15.mk

--- crdroid_a15.mk
+++ crdroid_a15.mk
@@ -1,6 +1,7 @@
-# Inherit the settings from the ROM itself.
-$(call inherit-product, vendor/crdroid/config/common.mk)
-
+# Inherit from the base device configuration first
 $(call inherit-product, device/samsung/a15/a15.mk)
+
+# Inherit the settings from the ROM itself (last, to allow overrides)
+$(call inherit-product, vendor/crdroid/config/common.mk)
 
 PRODUCT_NAME := crdroid_a15
