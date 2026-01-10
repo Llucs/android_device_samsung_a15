19b77719787cf2dd0040ba4fd76fe5efbffe4b54
Llucs <104036495+Llucs@users.noreply.github.com>
2026-01-09 21:26:52 -0300

    Atualizar o yaap_a15.mk

--- yaap_a15.mk
+++ yaap_a15.mk
@@ -1,7 +1,8 @@
-# Herdar as configurações da ROM YAAP
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
-
-# Herdar a base universal do dispositivo
+# Inherit from the base device configuration first
 $(call inherit-product, device/samsung/a15/a15.mk)
 
+# Inherit some common YAAP stuff (last, to allow overrides and extensions)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+
+# Device identifier / Product name for YAAP builds
 PRODUCT_NAME := yaap_a15
